e78444e063ae747d9edeab90ae097c7cc5b9232b
Viner108 <andrukovichl@mail.ru>
2023-04-19 19:32:40 +0300

    playlist commit

--- src/main/java/org/example/Playlist.java
+++ src/main/java/org/example/Playlist.java
@@ -1,28 +1,49 @@
 package org.example;
 
+import java.util.Random;
+
 public class Playlist {
     public static void main(String[] args) {
-        int[] array={1,2,3,4,5,6,8,9};
-        int sum=20;
-        int [] result=playlist(array,sum);
+        int[] array = new int[250];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(653) + 150;
+        }
+        int sum = 23721;
+        int difference = 4;
+        int[] result = playlist(array, sum, difference);
         printLines(result);
     }
-    public static int[] playlist(int[]array, int sum){
-        int play=0;
-        int [] temp =new int[array.length];
-        int j=0;
-        for (int i = 0; play<sum&&array[i]<=sum-play; i++) {
-            play=play+array[i];
-            temp[j]=array[i];
-            j++;
-            System.out.println(play);
-        }
-        int [] result=new int[j];
+
+    public static int[] playlist(int[] array, int sum, int difference) {
+        int play = 0;
+        int[] temp = new int[array.length];
+        int j = 0;
+            while (play < sum - difference || play > sum + difference) {
+                if (play < sum - difference && j < array.length) {
+                    Random random = new Random();
+                    int i = random.nextInt(250);
+                    play = play + array[i];
+                    temp[j] = array[i];
+                    j++;
+                }
+                if (play > sum + difference) {
+                    for (int i = 0; i <j; i++) {
+                        temp[i]=0;
+                    }
+                    play=0;
+                    j=0;
+                    System.out.println(temp[0]);
+                }
+                System.out.println(play);
+            }
+        int[] result = new int[j];
         for (int i = 0; i < j; i++) {
-            result[i]=temp[i];
+            result[i] = temp[i];
         }
         return result;
     }
+
     public static void printLines(int[] array) {//функция для выведения массива на консоль
         System.out.print("[");
         for (int i = 0; i < array.length; i++) {
