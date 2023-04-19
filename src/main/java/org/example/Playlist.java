348b71ef6a3aae3a6ab13350479fb6f3f8e5b0db
Viner108 <andrukovichl@mail.ru>
2023-04-19 20:00:22 +0300

    playlist without repetition commit

--- src/main/java/org/example/Playlist.java
+++ src/main/java/org/example/Playlist.java
@@ -10,30 +10,37 @@ public class Playlist {
             array[i] = random.nextInt(653) + 150;
         }
         int sum = 23721;
-        int difference = 4;
+        int difference = 2;
         int[] result = playlist(array, sum, difference);
         printLines(result);
     }
 
     public static int[] playlist(int[] array, int sum, int difference) {
         int play = 0;
+        int m=0;
         int[] temp = new int[array.length];
+        int[] index=new int[array.length];
         int j = 0;
             while (play < sum - difference || play > sum + difference) {
                 if (play < sum - difference && j < array.length) {
                     Random random = new Random();
                     int i = random.nextInt(250);
-                    play = play + array[i];
-                    temp[j] = array[i];
-                    j++;
+                    if (indexI(index, i)) {
+                        play = play + array[i];
+                        temp[j] = array[i];
+                        index[j] = i;
+                        j++;
+                    }
                 }
                 if (play > sum + difference) {
                     for (int i = 0; i <j; i++) {
                         temp[i]=0;
+                        index[i]=0;
                     }
                     play=0;
                     j=0;
                     System.out.println(temp[0]);
+                    m++;
                 }
                 System.out.println(play);
             }
@@ -41,7 +48,16 @@ public class Playlist {
         for (int i = 0; i < j; i++) {
             result[i] = temp[i];
         }
+        System.out.println(m);
         return result;
+    }
+    public static boolean indexI(int [] index,int i){
+        for (int j = 0; j <index.length; j++) {
+            if(index[j]==i){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void printLines(int[] array) {//функция для выведения массива на консоль
